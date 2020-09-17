0aca82901c12284f4e6d6e6a27fa78dd11ba3d47
diogosbr <diogosbr@gmail.com>
2020-09-17 18:14:48 -0300

    fix width slidebar

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,18 +14,18 @@ app_ui <- function(request) {
       sidebarPanel(
         textInput("robin1", "Robin 1L *", "A terra é plan...", width = "100%"),
         splitLayout(
-          sliderInput("rob.pos1_x", "Robin 1L (x)", 150, step = 10, min = 100, max = 400, width = "100px"),
-          sliderInput("rob.pos1_y", "Robin 1L (y)", 500, step = 10, min = 350, max = 600, width = "100px"),
+          sliderInput("rob.pos1_x", "x", 150, step = 10, min = 100, max = 400, width = "100px"),
+          sliderInput("rob.pos1_y", "y", 500, step = 10, min = 350, max = 600, width = "100px"),
           cellWidths = c("50%", "50%")
         ),
         
         
         
-        textInput("robin2", "Robin 2L", "", width = "400px"),
+        textInput("robin2", "Robin 2L", "", width = "100%"),
         splitLayout(
-          sliderInput("rob.pos2_x", "Robin 2L (x)", 150, step = 10, min = 100, max = 400, width = "100px"),
-          sliderInput("rob.pos2_y", "Robin 2L (y)", 470, step = 10, min = 350, max = 600, width = "100px"),
-          cellWidths = c("40%", "15%", "15%")
+          sliderInput("rob.pos2_x", "x", 150, step = 10, min = 100, max = 400, width = "100px"),
+          sliderInput("rob.pos2_y", "y", 470, step = 10, min = 350, max = 600, width = "100px"),
+          cellWidths = c("50%", "50%")
         ),
         
         # textInput("robin3", "Robin 3L", "", width = "400px"),
@@ -35,18 +35,18 @@ app_ui <- function(request) {
         #   cellWidths = c("40%", "15%", "15%")
         # ),
         
-        textInput("batman1", "Batman 1L *", "Cala a boca!", width = "400px"),
+        textInput("batman1", "Batman 1L *", "Cala a boca!", width = "100%"),
         splitLayout(
-          sliderInput("bat.pos1_x", "Batman 1L (x)", 430, step = 10, min = 350, max = 600, width = "100px"),
-          sliderInput("bat.pos1_y", "Batman 1L (y)", 500, step = 10, min = 350, max = 600, width = "100px"),
-          cellWidths = c("40%", "15%", "15%")
+          sliderInput("bat.pos1_x", "x", 430, step = 10, min = 350, max = 600, width = "100px"),
+          sliderInput("bat.pos1_y", "y", 500, step = 10, min = 350, max = 600, width = "100px"),
+          cellWidths = c("50%", "50%")
         ),
         
-        textInput("batman2", "Batman 2L", "", width = "400px"),
+        textInput("batman2", "Batman 2L", "", width = "100%"),
         splitLayout(
-          sliderInput("bat.pos2_x", "Batman 2L (x)", 430, step = 10, min = 350, max = 600, width = "100px"),
-          sliderInput("bat.pos2_y", "Batman 2L (y)", 460, step = 10, min = 350, max = 600, width = "100px"),
-          cellWidths = c("40%", "15%", "15%")
+          sliderInput("bat.pos2_x", "x", 430, step = 10, min = 350, max = 600, width = "100px"),
+          sliderInput("bat.pos2_y", "y", 460, step = 10, min = 350, max = 600, width = "100px"),
+          cellWidths = c("50%", "50%")
         ),
         
         # textInput("batman3", "Batman 3L", "", width = "400px"),
@@ -56,7 +56,7 @@ app_ui <- function(request) {
         #   cellWidths = c("40%", "15%", "15%")
         # ),
         
-        sliderInput("size", "Tamanho do texto", 1.4, step = 0.1, min = 0.1, max = 3, width = '150px'),
+        sliderInput("size", "Tamanho do texto", 1.4, step = 0.1, min = 0.1, max = 3, width = '100%'),
       ),
       mainPanel(imageOutput("meme"))
     )
